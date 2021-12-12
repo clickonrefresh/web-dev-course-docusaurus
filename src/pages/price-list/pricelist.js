b2d2e3bc25e311c706652683e0ca41c2c7e9bb0d
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-12-12 13:00:04 +0200

    Update and rename index.js to pricelist.js

---
+++ src/pages/price-list/pricelist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>Bonsai Price List</h1>
+      <p>December21</p>
+<iframe width="402" height="346" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=6992D7E136F5BA68%21415&authkey=%21AGrkEKTIWAv5Id8&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True"></iframe>
+	  </Layout>
+  );
+}
